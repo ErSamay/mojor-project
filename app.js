acafddcb0556d9b91d02e884ca58c97de369ebef
ErSamay <samayjain564@gmail.com>
2023-11-03 14:48:29 +0530

    add initial files

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ if(process.env.NODE_ENV != "production"){
   require('dotenv').config();
 }
 
-
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -21,17 +19,18 @@ const listingRouter = require("./router/listing.js")
 const reviewRouter = require("./router/review.js")
 const userRouter = require("./router/user.js")
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy  = require("passport-local");
 
 const User = require("./models/user.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb+srv://user123:user123@delta.s8kvkvv.mongodb.net/?retryWrites=true&w=majority"
 
 main()
   .then(() => {
-    console.log("connected to DB");
+    console.log("connection successfull");
   })
   .catch((err) => {
     console.log(err);
@@ -48,7 +47,20 @@ app.use(methodOverride("_method"))
 app.engine("ejs" , ejsMate);
 app.use(express.static(path.join(__dirname , "/public")));
 
+const store = MongoStore.create({
+  mongoUrl : MONGO_URL , 
+  crypto :{
+    secret : process.env.SECRET, 
+  },
+  touchAfter : 24 * 3600
+})
+
+store.on("error" , () => {
+  console.log("Error in Mongo Store" , err);
+});
+
 const sessionOptions = {
+  store : store , 
   secret : "mysupersecretstring" , 
   resave : false , 
   saveUninitialized : true,
@@ -58,6 +70,8 @@ const sessionOptions = {
     httpOnly : true,
   }
 }
+
+
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -69,8 +83,6 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 
-
-
 app.use((req , res , next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
@@ -78,151 +90,11 @@ app.use((req , res , next) => {
   next();
 
 } )
-// app.get("/demouser" , async(req , res) => {
-//   let fakeuser = new User({
-
-//     email : "Samay55@", 
-//     username : "Delta-student",
-
-//   });
-//   let registerUser = await User.register(fakeuser , "helloworld");
-//   res.send(registerUser);
-// })
-
-// app.get("/", (req, res) => {
-//   res.send("Hi, I am root");
-//   if(error){
-//     throw new ExpressError(400 , result.error)
-//   }
-//   else{
-//     next();
-//   }
-// });
-
-// const validateListing = (req , res , next ) => {
-//   let {error} = listingSchema.validate(req.body);
-
-//   if(error){
-//     let errMsg = error.details.map((el) => el.message).join(",");
-//     throw new ExpressError(400 , result.error)
-//   }
-//   else{
-//     next();
-//   }
-// }
-
-// const validateReview = (req , res , next ) => {
-//   let {error} = reviewSchema.validate(req.body);
-
-//   if(error){
-//     let errMsg = error.details.map((el) => el.message).join(",");
-//     throw new ExpressError(400 , result.error)
-//   }
-//   else{
-//     next();
-//   }
-// }
 
 app.use("/listings" , listingRouter);
 app.use("/listings/:id/reviews" , reviewRouter);
 app.use("/" , userRouter);
 
-// //Index Route
-// app.get("/listings",wrapAsync (async (req, res ,next) => {
-//   const allListings = await Listing.find({});
-//   res.render("listings/index.ejs", { allListings });
-// }));
-
-// //New Route
-// app.get("/listings/new", (req, res) => {
-//   res.render("listings/new.ejs");
-// });
-
-
-
-// //Show Route
-// app.get("/listings/:id",wrapAsync( async (req, res , next) => {
-//   let { id } = req.params;
-//   const listing = await Listing.findById(id).populate("reviews");
-//   res.render("listings/show.ejs", { listing });
-// }));
-
-// //Create Route
-// app.post("/listings", validateListing ,wrapAsync (
-//    async (req, res , next) => {
-//   const newListing = new Listing(req.body.listing);
-//   await newListing.save();
-//   res.redirect("/listings");
-
-// }))
-// app.get("/listings/:id/edit" , wrapAsync(async(req , res , next) => {
-//     let{id} = req.params;
-//     const listing = await Listing.findById(id);
-//     res.render("listings/edit.ejs", { listing });
-    
-// }))
-
-// //Edit Route
-// // app.get("/listings/:id/edit", async (req, res) => {
-// //   let { id } = req.params;
-// //   const listing = await Listing.findById(id);
-// //   res.render("listings/edit.ejs", { listing });
-// // });
-
-// // //Update Route
-// app.put("/listings/:id",  (req, res , next) => {
-//   let { id } = req.params;
-// Listing.findByIdAndUpdate(id, { ...req.body.listing }).then((res) => {
-//   console.log(res)
-// })
-// .catch((err) => {
-//   console.log(err)
-// })
-//   res.redirect(`/listings/${id}`);
-// })
-
-// // //Delete Route
-// app.delete("/listings/:id",wrapAsync (async (req, res , next) => {
-
-//   let { id } = req.params;
-//   let deletedListing = await Listing.findByIdAndDelete(id);
-//   console.log(deletedListing);
-//   res.redirect("/listings");
-// }))
-
-// app.post("/listings/:id/reviews" ,validateReview ,wrapAsync(  async(req , res) => {
-//   // let{id} = req.params;
-//  let listing =  await Listing.findById(req.params.id);
-//  let newReview = new Review(req.body.review)
-// listing.reviews.push(newReview);
-// await newReview.save();
-// await listing.save();
-
-// res.redirect(`/listings/${listing._id}`)
-
-// }))
-
-// app.delete("/listings/:id/reviews/:reviewId" , wrapAsync(async(req , res) => {
-// let {id , reviewId} = req.params;
-// await Listing.findByIdAndUpdate(id , {$pull : {reviews : reviewId}})
-//  await Review.findByIdAndDelete(reviewId);
-//  res.redirect(`/listings/${id}`)
-// })  )
-
-
-// app.get("/testListing", async (req, res) => {
-//   let sampleListing = new Listing({
-//     title: "My New Villa",
-//     description: "By the beach",
-//     price: 1200,
-//     location: "Calangute, Goa",
-//     country: "India",
-//   });
-
-//   await sampleListing.save();
-//   console.log("sample was saved");
-//   res.send("successful testing");
-// });
 app.all("*" , (req ,res , next) => {
   next(new ExpressError(404 , "Error Encountered"))
 })
